4daef3f975a55e02a4a663c11e9d3733bd4d52a9
Regoradin <oliver@theabates.com>
2017-01-13 23:10:47 -0600

    The Very Basics
    Added the actual Unity file, build a test scene, really basic character
    controls, a decent gradient texture, and the basics of a lighting and
    camera rig to augment the gradient.

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+
+	private Light lightbulb;
+	private Vector3 endpoint_position;
+	private GameObject floor;
+
+	void Start () {
+
+		lightbulb = GetComponent<Light>();
+
+		//looks for an object called "Endpoint" for the  position. If it doesn't exist, sets the position equal to 0
+		if (GameObject.Find("Endpoint"))
+		{
+			endpoint_position = GameObject.Find("Endpoint").transform.position;
+		}
+		else
+		{
+			endpoint_position = Vector3.zero;
+		}
+
+		//looks for the floor, if not logs an error before an inevitable crash
+		floor = GameObject.Find("Floor");
+
+	}
+	
+	void Update () {
+
+		//changes the intesity of the light with the distance between the player and the endpoint as compared with the size of the level, which is the scale of the floor in the x direction for now
+		float intensity = floor.transform.localScale.x/(Vector3.Distance(endpoint_position, transform.parent.transform.position));
+
+		lightbulb.intensity = intensity;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float rotation_angle;
+
+	private CharacterController controller;
+	private GameObject body;
+
+	void Start () {
+
+		controller = GetComponent<CharacterController>();
+		body = GameObject.Find("Body");
+
+	}
+	
+	void Update () {
+
+		//Handles player movement
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+		movement *= speed;
+		movement += Physics.gravity;
+
+		controller.Move(movement);
+
+		//Rotates the player by rotation angle when the character is pressing movement keys
+		Vector3 rotation = Vector3.zero;
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			rotation.z = -rotation_angle;
+		}
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			rotation.z = rotation_angle;
+		}
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			rotation.x = rotation_angle;
+		}
+		if (Input.GetAxis("Vertical") < 0)
+		{
+			rotation.x = -rotation_angle;
+		}
+
+		body.transform.localEulerAngles = rotation;
+	
+	}
+}
